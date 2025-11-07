7b8fe6a9ff5cca57c1bb5c3412e946c0610629cf
Kunal Gajanan Adkine <kunaladkine90@gmail.com>
2025-11-07 11:45:33 +0530

    Initial commit

---
+++ Day 1 (Create Alter Modify Data Type).sql
@@ -0,0 +1,20 @@
+-- create database school;
+-- use school;
+-- create table student(st_id INT,Sname VARCHAR(20), class INT,Gender char(1));
+-- use school;
+-- alter table student add phoneno varchar(10);
+-- alter table student add address varchar(40);
+-- select * from school.student;
+
+-- use movies;
+-- create table Bmovies(Mname varchar(30),Mrating INT,Mdata DATE);
+-- select * from movies.bmovies;
+-- alter table Bmovies add MCategory varchar(20);
+-- select * from movies.bmovies;
+
+-- alter table Bmovies modify Mrating char(5);
+-- select * from movies.Bmovies;
+
+ -- create table Hmovies(Mname varchar(30), Mrating char(1), Mdate Date, Mcategory varchar(30));
+-- select * from movies.hmovies;
+

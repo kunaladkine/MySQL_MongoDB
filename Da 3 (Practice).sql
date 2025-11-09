77aae154c44dac6916e84f364b2df9eab878b1db
Kunal Gajanan Adkine <kunaladkine90@gmail.com>
2025-11-09 21:28:09 +0530

    Create Da 3 (Practice).sql
    practice

---
+++ Da 3 (Practice).sql
@@ -0,0 +1,13 @@
+-- drop database moviesdb; -- DATABASE HAS DELETED
+
+-- CREATE A NEW DATABASE
+-- create database movies;
+
+-- use movies;
+-- CREATE A TABLE OF HMOVIES
+-- create table hmovies(hname varchar(20),hrateing INT);
+
+-- INSERTING THE TABLE DATA 
+-- insert hmovies values("Iron Man",4)
+
+-- create table 
